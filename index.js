6274ec435b0117c6822a7c0f3d343ae2af02b17e
Bradley Fargo <blewfargs@gmail.com>
2018-11-08 16:33:49 -0500

    converted to express application

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.sendFile('index.html'));
+
+app.use(express.static(__dirname + '/view')); // store html files in view folder
+app.use(express.static(__dirname + '/script')); // store js&css in script folder
+
+app.get('/', (req, res) => res.sendFile('index.html', {
+  root: __dirname
+}));
+
+app.get(__dirname + '/sitemap', (req, res) => res.sendFile('/sitemap.html'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
