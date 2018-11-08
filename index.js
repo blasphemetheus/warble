4879b0e902657adb90f2d4c4b12577a173c16642
Bradley Fargo <blewfargs@gmail.com>
2018-11-08 17:14:29 -0500

    added examples, need to add dependencies

--- index.js
+++ index.js
@@ -2,6 +2,71 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const mongoose = require('mongoose');
+
+//
+
+mongoose.connect('mongodb://localhost/my_database');
+
+// ------------ examples -- look at https://www.npmjs.com/package/mongoose
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const BlogPost = new Schema({
+  author: ObjectId,
+  title: String,
+  body: String,
+  date: Date
+});
+
+// --
+const Comment = new Schema({
+  name: {
+    type: String,
+    default: 'hahaha'
+  },
+  age: {
+    type: Number,
+    min: 18,
+    index: true
+  },
+  bio: {
+    type: String,
+    match: /[a-z]/
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  buff: Buffer
+});
+// setter
+Comment.path('name').set((v) =>
+  return capitalize(v));
+//middleware
+Comment.pre('save', (next) => {
+  notify(this.get('email'));
+  next();
+});
+
+//
+
+const myModel = mongoose.model('ModelName', mySchema); // accessing a model you
+
+//
+
+const instance = new MyModel();
+instance.my.key = 'ahoy';
+instance.save((err) => console.error(err));
+
+//
+
+MyModel.find({}, (err, docs) {
+  // docs.forEach basically
+});
+
+
+// ------------ end of examples
 
 app.use(express.static(__dirname + '/view')); // store html files in view folder
 app.use(express.static(__dirname + '/script')); // store js&css in script folder
